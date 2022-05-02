900db5b7309c8b5cfd508485896a4881a4e4604a
Alfred.Qiu <sc941203@gmail.com>
2022-05-02 16:58:02 +0800

    resite

--- .umirc.js
+++ .umirc.js
@@ -3,7 +3,7 @@ import path from 'path';
 export default {
   history: { type: 'hash' },
   base: '/',
-  publicPath: process.env.DOCS_ENV === 'prod' ? 'https://alfred-sg.github.io/react-leaflet2/' : '/',
+  publicPath: process.env.DOCS_ENV === 'prod' ? 'https://schifred.github.io/react-leaflet2/' : '/',
   mode: 'site',
   logo: 'https://smebimage.fuliaoyi.com/Fger7VZclDUaXDJuqg42MlsUqV-w',
   favicon: 'https://smebimage.fuliaoyi.com/Fger7VZclDUaXDJuqg42MlsUqV-w',
